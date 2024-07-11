a43aa25942ee8ad7512f30e4ac16d07168c17df7
mahmutegribck <megribck01@gmail.com>
2024-07-11 15:42:29 +0300

    Added ValidationExtension.

---
+++ ChatAppAPI/Extensions/ValidationExtension.cs
@@ -0,0 +1,21 @@
+﻿using ChatAppAPI.Servisler.Mesajlar.DTOs;
+using ChatAppAPI.Servisler.OturumYonetimi.DTOs;
+using FluentValidation;
+
+namespace ChatAppAPI.Extensions
+{
+    public static class ValidationExtension
+    {
+        public static IServiceCollection AddValidationExtension(this IServiceCollection services)
+        {
+            services.AddScoped<IValidator<MesajGonderDTO>, MesajGonderDTOValidator>();
+            services.AddScoped<IValidator<MesajlariGorulduYapDTO>, MesajlariGorulduYapDTOValidator>();
+
+            services.AddScoped<IValidator<KullaniciKayitDto>, KullaniciKayitDtoValidator>();
+            services.AddScoped<IValidator<KullaniciGirisDto>, KullaniciGirisDtoValidator>();
+            services.AddScoped<IValidator<KullaniciAdiIleGirisYapDTO>, KullaniciAdiIleGirisYapDTOValidator>();
+
+            return services;
+        }
+    }
+}
